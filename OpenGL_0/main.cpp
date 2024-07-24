1c9a26768efd31c24f82d71203f5b1bfb2db9300
Ivan Rykov <derauferstanden@gmail.com>
2024-07-24 21:39:57 +0300

    Re Initial
    - GLFW
    - glad

--- OpenGL_0/main.cpp
+++ OpenGL_0/main.cpp
@@ -1,123 +1,48 @@
-#include <GLFW/glfw3.h>
-#include <windows.h>
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 
-float vertex[] = { -1,-1,0, 1,-1,0, 1,1,0 , -1,1,0 };
-float normal[] = { -1,-1,3, 1,-1,3, 1,1,3 , -1,1,3 };
 
-void DrawRectangle()
+GLFWwindow* CreateWindow()
 {
-    glNormal3f(0, 0, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_NORMAL_ARRAY);
+	GLFWwindow* result = glfwCreateWindow(640, 640, "OpenGL_0", NULL, NULL);
 
-        glVertexPointer(3, GL_FLOAT, 0, vertex);
-        glNormalPointer(GL_FLOAT, 0, normal);
-
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_NORMAL_ARRAY);
+	return result;
 }
 
-void DrawVisibleCube()
+int main()
 {
-    glPushMatrix();
+	glfwInit();
 
-    glColor3f(1, 0, 0);
-    DrawRectangle();
-    
-    glRotatef(-90, 1, 0, 0);
-    glTranslatef(0, 1, -1);
+	GLFWwindow* window = CreateWindow();
 
-    glColor3f(0, 1, 0);
-    DrawRectangle();
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    glRotatef(-90, 0, 1, 0);
-    glTranslatef(1, 0, 1);
+	glfwMakeContextCurrent(window);
 
-    glColor3f(0, 0, 1);
-    DrawRectangle();
+	gladLoadGL();
 
-    glPopMatrix();
-}
+	glViewport(0, 0, 800, 800);
 
-int main(void)
-{
-    GLFWwindow* window;
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 800, "OpenGLTest", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
-    glfwMakeContextCurrent(window);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-0.1, 0.1, -0.1, 0.1, 0.2, 1000);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glEnable(GL_DEPTH_TEST);
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    glEnable(GL_COLOR_MATERIAL);
-
-    glEnable(GL_NORMALIZE);
-
-    float lightAngle = 0.0f;
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glClearColor(0.0f, 0.0f, 0.0f, 0);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glPushMatrix();
-        glTranslatef(0, 0, -3);
-
-        glScalef(0.5f, 0.5f, 0.5f);
-
-        glRotatef(-60, 1, 0, 0);
-        glRotatef(60, 0, 0, 1);
-        glTranslatef(0.3f, 0.3f, 0);
-
-        DrawVisibleCube();
-
-        glPushMatrix();
-            glTranslatef(0, 0, -1);
-
-            glRotatef(lightAngle, 0, 1, 0);
-            lightAngle += 1;
-
-            float lightPosition[] = { 0,0,2,0 };
-            glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-
-            glTranslatef(0, 0, 2);
-            glScalef(0.2f, 0.2f, 0.2f);
-            glColor3f(1, 1, 1);
-            DrawRectangle();
-        glPopMatrix();
-
-        glPopMatrix();
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-
-        Sleep(1);
-    }
-
-    glfwTerminate();
-    return 0;
+	return 0;
 }
